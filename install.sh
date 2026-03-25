3d40a044cfea44da5036d8674a2cef040e564d74
Forbirdden <102984166+Forbirdden@users.noreply.github.com>
2026-03-25 19:51:13 +0500

    Now there are 3 different machines...

--- install.sh
+++ install.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 
 sudo dnf config-manager setopt fedora-cisco-openh264.enabled=0
-
 sudo dnf clean all
-
 sudo dnf install -y ansible-core
-
 ansible-galaxy collection install community.general
 
-ansible-playbook playbook.yml --ask-become-pass -i "localhost," -c local
+echo "Select playbook"
+options=("Workstation" "Survivor Laptop" "Veteran PC" "Exit")
+
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Workstation")
+            PLAYBOOK="workstation.yml"
+            break
+            ;;
+        "Survivor Laptop")
+            PLAYBOOK="laptop.yml"
+            break
+            ;;
+        "Veteran PC")
+            PLAYBOOK="oldpc.yml"
+            break
+            ;;
+        "Exit")
+            exit 0
+            ;;
+        *) echo "WHOA wHat are YOU doinG?";;
+    esac
+done
+
+ansible-playbook "$PLAYBOOK" --ask-become-pass -i "localhost," -c local
